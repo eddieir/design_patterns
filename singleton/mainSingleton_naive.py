97f1b1b96cc6f812362d4e1f779998899593c21e
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2019-11-27 14:37:26 +0100

    craetional and structural patterns done

---
+++ singleton/mainSingleton_naive.py
@@ -0,0 +1,49 @@
+"""
+
+
+Singleton is a creational design pattern, which ensures that only one object of its kind exists and provides a single point of access to it for any other code.
+
+Singleton has almost the same pros and cons as global variables. Although they’re super-handy, they break the modularity of your code.
+
+You can just use a class, which depends on Singleton, in some other context. You’ll have to carry the Singleton class as well. Most of the time, this limitation comes up during the creation of unit tests.
+
+"""
+from __future__ import annotations
+from typing import Optional
+
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instance: Optional[Singleton] = None
+
+    def __call__(self) -> Singleton:
+        if self._instance is None:
+            self._instance = super().__call__()
+        return self._instance
+
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+
+        # ...
+
+
+if __name__ == "__main__":
+    # The client code.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
